60a433b7b99fd44b7ba3d3fc57414dad4e251597
Noah Burton <nburton115@gmail.com>
2023-08-26 15:29:27 -0600

    class square and svg

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const Shape = require('./lib/shapes.js');
 const Circle = require('./lib/circle.js');
+const Square = require('./lib/square.js');
 
 const isValidHexcode = hexCode => {
     const hexRules = /^#?([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/; // chat GPT wrote this check for me 
@@ -118,7 +119,11 @@ inquirer
             const generatedSvG= circleInstance.renderCircleSVG();
             console.log(generatedSvG);
             writeSvg('icon.svg', generatedSvG);
-        }
+        } if (data.shape === 'Square') {
+            const squareInstance = new Square(characters, textColor, shapeColor, shape);
+            const generatedSvG = squareInstance.renderSquareSVG();
+            writeSvg('icon.svg', generatedSvG);
+        } else { console.log('triangle')}
 
        
 
